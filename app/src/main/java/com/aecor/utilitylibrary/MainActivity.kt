38806b0493c3639237212edb399af24300ce45d1
indrajit2018 <39183192+indrajit2018@users.noreply.github.com>
2022-02-08 17:39:22 +0530

    First commit

---
+++ app/src/main/java/com/aecor/utilitylibrary/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aecor.utilitylibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//https://www.geeksforgeeks.org/how-to-create-your-own-android-library-and-publish-it-in-github/
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ utillibrary/src/main/java/com/aecor/utillibrary/Utility.kt
@@ -0,0 +1,14 @@
+package com.aecor.utillibrary
+
+import java.util.regex.Pattern
+
+object Utility {
+
+    fun isEmailValid(email: String): Boolean {
+        val expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"
+        val pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+
+}
